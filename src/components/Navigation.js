18638ebc15a857055f07457815f8e8dae045895b
TerSimaN <mcsiman998@gmail.com>
2022-01-20 15:03:50 +0200

    added color changer

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,15 @@
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faToggleOn, faToggleOff } from '@fortawesome/free-solid-svg-icons';
 
 function Navigation(props) {
+
+    const [color, toggleColor] = useState(true);
+
     return (
         <>
-            <Navbar bg="light" variant="light">
+            <Navbar bg={color ? 'light' : 'dark'} variant={color ? 'light' : 'dark'}>
                 <Container>
                     <Navbar.Brand>Navbar</Navbar.Brand>
                     <Nav className="me-auto">
@@ -11,6 +17,10 @@ function Navigation(props) {
                         <Nav.Link>Foods</Nav.Link>
                         <Nav.Link>Breweries</Nav.Link>
                     </Nav>
+                    <FontAwesomeIcon
+                        className={color ? 'text-dark' : 'text-light'}
+                        onClick={() => toggleColor(!color)} 
+                        icon={color ? faToggleOn : faToggleOff} />
                 </Container>
             </Navbar>
         </>
