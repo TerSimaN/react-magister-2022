22cc7ef62e5771631b4184383dc3edf92867e642
TerSimaN <mcsiman998@gmail.com>
2022-01-25 17:26:59 +0200

    updated breweries added axios

--- src/pages/Breweries.js
+++ src/pages/Breweries.js
@@ -1,51 +1,27 @@
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
-import BreweriesListItem from "../components/breweries/BreweriesListItem";
-import BreweriesGridItem from "../components/breweries/BreweriesGridItem";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import BreweriesList from "../components/breweries/BreweriesList";
 
 function Breweries(props) {
+
+    const [breweries, setBreweries] = useState([]);
+
+    function fetchBreweries() {
+        axios.get('https://api.openbrewerydb.org/breweries')
+        .then(function (response) {
+            setBreweries(response.data);
+        });
+    }
+
+    useEffect(() => {
+        fetchBreweries();
+    }, []);
+
     return (
         <>
             <Container>
-                <Row className="justify-content-md-center">
-                    <Col md={3}>
-                        <Form>
-                            <Form.Group className="mb-3" controlId="formBreweryCity">
-                                <Form.Label>Brewery City</Form.Label>
-                                <Form.Control type="text" placeholder="Enter city" />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBreweryName">
-                                <Form.Label>Brewery Name</Form.Label>
-                                <Form.Control type="text" placeholder="Enter name" />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBreweryState">
-                                <Form.Label>Brewery State</Form.Label>
-                                <Form.Control type="text" placeholder="Enter state" />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formSelectBreweryType">
-                                <Form.Label>Select Brewery Type</Form.Label>
-                                <Form.Select>
-                                    <option value="micro">Micro brewery</option>
-                                    <option value="nano">Nano brewery</option>
-                                    <option value="regional">Regional brewery</option>
-                                    <option value="brewpub">Brewpub brewery</option>
-                                    <option value="large">Large brewery</option>
-                                    <option value="planning">Planning brewery</option>
-                                    <option value="bar">Bar brewery</option>
-                                    <option value="contract">Contract brewery</option>
-                                    <option value="proprietor">Proprietor brewery</option>
-                                    <option value="closed">Closed brewery</option>
-                                </Form.Select>
-                            </Form.Group>
-                            <Button variant="success" type="submit">Filter breweries</Button>
-                        </Form>
-                    </Col>
-                    <Col md={6}>
-                        <Row>
-                            <BreweriesGridItem />
-                            <BreweriesListItem />
-                        </Row>
-                    </Col>
-                </Row>
+                <BreweriesList  breweries={breweries}/>
             </Container>
         </>
     )
