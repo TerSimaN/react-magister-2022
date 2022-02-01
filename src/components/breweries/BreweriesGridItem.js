c5cafefcd23b0f7465c122b6230f8de257bf07e3
TerSimaN <mcsiman998@gmail.com>
2022-02-01 20:18:18 +0200

    updated, added toggle theme and styling

--- src/components/breweries/BreweriesGridItem.js
+++ src/components/breweries/BreweriesGridItem.js
@@ -1,18 +1,36 @@
 import { Card, Col, ListGroup, ListGroupItem } from "react-bootstrap";
+import { useTheme } from "../../ThemeContext";
 
 function BreweriesGridItem({brewery}) {
+
+    const [theme] = useTheme();
+
     return (
         <>
             <Col md={4}>
-                <Card className="mx-auto my-2" style={{ width: '16rem' }}>
+                <Card className="card-element"
+                    bg={theme ? "dark" : "light"}
+                    text={theme ? "light" : "dark"}>
                     <Card.Header as="h5">Brewery: "{brewery.name}"</Card.Header>
                     <ListGroup variant="flush">
-                        <ListGroupItem>Type: "{brewery.brewery_type}"</ListGroupItem>
-                        <ListGroupItem>City: {brewery.city}</ListGroupItem>
-                        <ListGroupItem>State: {brewery.state === null ? "None" : brewery.state}</ListGroupItem>
-                        <ListGroupItem>Country: {brewery.country}</ListGroupItem>
-                        <ListGroupItem style={{ textAlign: 'center' }}>
-                            <Card.Link href={brewery.website_url}>Website Link</Card.Link>
+                        <ListGroupItem variant={theme ? "dark" : "light"}>
+                            Type: "{brewery.brewery_type}"
+                        </ListGroupItem>
+                        <ListGroupItem variant={theme ? "dark" : "light"}>
+                            City: {brewery.city}
+                        </ListGroupItem>
+                        <ListGroupItem variant={theme ? "dark" : "light"}>
+                            State: {brewery.state === null ? "None" : brewery.state}
+                        </ListGroupItem>
+                        <ListGroupItem variant={theme ? "dark" : "light"}>
+                            Country: {brewery.country}
+                        </ListGroupItem>
+                        <ListGroupItem 
+                            className="list-group-item-link"
+                            variant={theme ? "dark" : "light"}>
+                            <Card.Link href={brewery.website_url}>
+                                Website Link
+                            </Card.Link>
                         </ListGroupItem>
                     </ListGroup>
                 </Card>
