6105411b1b7b729936f8a2680dee376ed0a8c88f
TerSimaN <mcsiman998@gmail.com>
2022-02-04 15:56:57 +0200

    updated, added 'BreweryProvider'

--- src/pages/Breweries.js
+++ src/pages/Breweries.js
@@ -1,36 +1,23 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import BreweriesList from "../components/breweries/BreweriesList";
 import BreweryFilters from "../components/filters/BreweryFilters";
+import { BreweryProvider } from "../components/breweries/BreweryContext";
 
 function Breweries(props) {
-
-    const [breweries, setBreweries] = useState([]);
-
-    function fetchBreweries() {
-        axios.get('https://api.openbrewerydb.org/breweries')
-        .then(function (response) {
-            setBreweries(response.data);
-        });
-    }
-
-    useEffect(() => {
-        fetchBreweries();
-    }, []);
-
     return (
         <>
-            <Container>
-                <Row className="justify-content-md-center">
-                    <Col md={3}>
-                        <BreweryFilters />
-                    </Col>
-                    <Col md={8}>
-                        <BreweriesList  breweries={breweries}/>
-                    </Col>
-                </Row>
-            </Container>
+            <BreweryProvider>
+                <Container>
+                    <Row className="justify-content-md-center">
+                        <Col md={3}>
+                            <BreweryFilters />
+                        </Col>
+                        <Col md={8}>
+                            <BreweriesList />
+                        </Col>
+                    </Row>
+                </Container>
+            </BreweryProvider>
         </>
     )
 }
