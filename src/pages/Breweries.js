98c82aa73a1932af164e42c7d7f6dd9091af0270
TerSimaN <mcsiman998@gmail.com>
2022-02-06 20:10:40 +0200

    removed 'BreweryProvider'

--- src/pages/Breweries.js
+++ src/pages/Breweries.js
@@ -1,23 +1,20 @@
 import { Col, Container, Row } from "react-bootstrap";
 import BreweriesList from "../components/breweries/BreweriesList";
 import BreweryFilters from "../components/filters/BreweryFilters";
-import { BreweryProvider } from "../components/breweries/BreweryContext";
 
 function Breweries(props) {
     return (
         <>
-            <BreweryProvider>
-                <Container>
-                    <Row className="justify-content-md-center">
-                        <Col md={3}>
-                            <BreweryFilters />
-                        </Col>
-                        <Col md={8}>
-                            <BreweriesList />
-                        </Col>
-                    </Row>
-                </Container>
-            </BreweryProvider>
+            <Container>
+                <Row className="justify-content-md-center">
+                    <Col md={3}>
+                        <BreweryFilters />
+                    </Col>
+                    <Col md={8}>
+                        <BreweriesList />
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
